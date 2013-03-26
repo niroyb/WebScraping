6f06d811a37f0c48cea3b38b739e7293814723a6
Nicolas Roy <niroyb@gmail.com>
2013-03-26 13:36:22 -0400

    Added Scrap97Things

---
+++ Scrap_97TingsProgrammer.py
@@ -0,0 +1,43 @@
+import scraptools
+from urllib2 import urlparse
+from lxml import etree
+
+head = '''<html>
+<head>
+<style TYPE="text/css">
+body, p, div, li, td { font-family:Verdana,Arial,Helvetica,sans-serif; color:#222222; font-size:11px; }
+h1 {page-break-before: always;}
+</style>
+</head>
+<body>
+'''
+
+def getHTMLContent(url):
+    elems = scraptools.getElementsFromUrl(url, '#content')
+    content = elems[0]
+    return etree.tostring(content, pretty_print=True, method="html")
+    
+
+def scrapOreily(indexUrl, outName):
+    links = scraptools.getElementsFromUrl(url, '#bodyContent ol a:nth-child(1)')
+    
+    f = open(outName, 'w')
+    
+    f.write(head)
+    
+    f.write(getHTMLContent(indexUrl))
+    
+    for link in links:
+        relativeLink = link.get('href')
+        print relativeLink
+        absoluteLink = urlparse.urljoin(url, relativeLink)
+        
+        f.write(getHTMLContent(absoluteLink))
+        
+    f.write('</body></html>')
+    f.close()
+
+url = ('http://programmer.97things.oreilly.com/wiki/index.php/'
+       'Contributions_Appearing_in_the_Book')
+
+scrapOreily(url, '97 things a programmer should know.html')
